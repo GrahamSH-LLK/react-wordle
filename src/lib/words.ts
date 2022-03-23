1505e032fb3e29eacd583899e9e801b6f371162e
GrahamSH <grahamshllk@gmail.com>
2022-03-23 18:26:02 +0000

    Fin(ish)

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -76,12 +76,12 @@ export const localeAwareUpperCase = (text: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = new Date(2022, 2, 7).valueOf()
+  const epochMs = new Date(2022, 2, 23).valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
-
+  
   return {
     solution: localeAwareUpperCase(WORDS[index % WORDS.length]),
     solutionIndex: index,
